b54bbb1df7c2a2670fcca024698ac805381ccf33
Ulydev <ulydev@live.fr>
2017-06-11 23:56:37 +0200

    credit

--- lue.lua
+++ lue.lua
@@ -1,6 +1,6 @@
 -- lue.lua v0.1
 
--- Copyright (c) 2016 Ulysse Ramage
+-- Copyright (c) 2017 Ulysse Ramage
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 -- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -24,7 +24,9 @@ local function lerp(a, b, k) --smooth transitions
   end
 end
 
-local function HSL(h, s, l, a) --where the magic happens
+--by Taehl https://love2d.org/wiki/HSL_color
+--where the magic happens
+local function HSL(h, s, l, a)
   if s<=0 then return l,l,l,a end
   h, s, l = (h%255)/256*6, s/255, l/255
   local c = (1-math.abs(2*l-1))*s
